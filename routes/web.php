7a87bfef1a8e063b5dad0b8e253bf9773883f311
Kevin Eduardo Hernández Ruiz <136494407+Code-Wizard02@users.noreply.github.com>
2024-06-24 22:35:18 -0600

    Register y Login Funcionales (#2)
    * Livewire y tailwind agregado, register version 1
    
    * Agregando estructura de proyecto y librerias necesarias
    
    * Register
    
    * creacion de login
    
    ---------
    
    Co-authored-by: Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
    Co-authored-by: Shalom <tjoelshalom@gmail.com>
    Co-authored-by: ShalomThz <136143013+ShalomThz@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Livewire\Login;
+use App\Livewire\Register;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return Auth::user();
+    })->name('home');
 });
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', Login::class)->name('login');
+    Route::get('/registrarse', Register::class)->name('register');
+});
+
